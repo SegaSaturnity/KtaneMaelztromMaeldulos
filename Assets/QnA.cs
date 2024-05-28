78776de5f407d04ca251ec6bbe486b2bce1029b6
ObjectsCountries <oandcofficial@gmail.com>
2024-05-27 19:08:05 -0700

    Add TP and (almost done w/) ruleseed support for Q & A

--- Assets/QnA.cs
+++ Assets/QnA.cs
@@ -9,6 +9,7 @@ public class QnA : MonoBehaviour {
     public KMBombModule module;
     public KMBombInfo bomb;
     public KMAudio audio;
+    public KMRuleSeedable rs;
 	
 	private bool isSolved;
 	private static int moduleCount;
@@ -26,8 +27,18 @@ public class QnA : MonoBehaviour {
 	public TextMesh display_text;
 
 	void Start () {
+	    var RND = rs.GetRNG();
+	    if(RND.Seed != 1){
+	        int[] numbers = new int[]{0, 1, 2, 3, 4, 5, 6, 7};
+	        for(int i = 0; i < 6; i++){
+	            RND.ShuffleFisherYates(numbers);
+	            for(int j = 0; j < 8; j++){
+	                table[j * 6 + i] = numbers[j];
+	            }
+	        }
+	    }
 		moduleId = moduleCount++;
-		Debug.LogFormat("[Q&A #{0}] Module started.", moduleId);
+		Debug.LogFormat("[Q & A #{0}] Module started.", moduleId);
 		
 		c_answers = (string[])answers.Clone();
 		c_questions = (string[])questions.Clone();
@@ -35,8 +46,8 @@ public class QnA : MonoBehaviour {
 		Shuffle(questions);
 		Shuffle(answers);
 		
-		Debug.LogFormat("[Q&A #{0}] Starting board: {1}", moduleId, string.Join(", ", answers));
-		Debug.LogFormat("[Q&A #{0}] First press should be {1}", moduleId, answers[7]);
+		Debug.LogFormat("[Q & A #{0}] Starting board: {1}", moduleId, string.Join(", ", answers));
+		Debug.LogFormat("[Q & A #{0}] First press should be {1}", moduleId, answers[7]);
 		
 		foreach (KMSelectable button in b_answers){
 			int i = System.Array.IndexOf(b_answers, button);
@@ -47,13 +58,13 @@ public class QnA : MonoBehaviour {
 	}
 
 	private void pressAnswer ( int pos ) {
+		b_answers[pos].AddInteractionPunch();
 		if (isSolved) return;
 		
-		b_answers[pos].AddInteractionPunch();
 		string ans_press = b_answers[pos].transform.GetChild(0).GetComponent<TextMesh>().text;
 		
 		if (display_text.text == "" && pos != 7) {
-			Debug.LogFormat("[Q&A #{0}] First press should be bottom right. Strike.", moduleId);
+			Debug.LogFormat("[Q & A #{0}] First press should be bottom right. Strike.", moduleId);
 			module.HandleStrike();
 			return;
 		}	
@@ -63,16 +74,16 @@ public class QnA : MonoBehaviour {
 			int z = System.Array.IndexOf(c_answers, ans_press);
 			
 			if (table[y * 6 + x] != z) {
-				Debug.LogFormat("[Q&A #{0}] Pressed {1}, expected {2} on question {3}", moduleId, ans_press, c_answers[table[y * 6 + x]], display_text.text);
+				Debug.LogFormat("[Q & A #{0}] Pressed {1}, expected {2} on question {3}", moduleId, ans_press, c_answers[table[y * 6 + x]], display_text.text);
 				module.HandleStrike();
 				return;
 			}
-			Debug.LogFormat("[Q&A #{0}] Pressed {1}, on question {2}, correct.", moduleId, ans_press, display_text.text);
+			Debug.LogFormat("[Q & A #{0}] Pressed {1}, on question {2}, correct.", moduleId, ans_press, display_text.text);
 		}
 		
 		audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.CorrectChime, transform);
 		if (questions.Length == 0) {
-			Debug.LogFormat("[Q&A #{0}] Answered all the questions, module solved!", moduleId);
+			Debug.LogFormat("[Q & A #{0}] Answered all the questions, module solved!", moduleId);
 			isSolved = true;
 			module.HandlePass();
 			return;
@@ -91,4 +102,36 @@ public class QnA : MonoBehaviour {
             arr[r] = tmp;
         }
 	}
+
+    string TwitchHelpMessage = "!{0}, then a word to press the corresponding button. For example, !{0} THERE to press the THERE button.";
+    string TwitchManualCode = "https://ktane.timwi.de/HTML/Q%20%26%20A.html";
+
+	IEnumerator ProcessTwitchCommand(string command)
+    {
+        yield return null;
+        command = command.ToUpperInvariant();
+        if(!answers.Contains(command)){
+            yield return "sendtochaterror {0}, your command must consist of one of the following: THAT, THEN, THERE, THEM, THEY, THIS, THESE, THOSE.";
+        }else{
+            yield return b_answers.Where(x => x.GetComponentInChildren<TextMesh>().text == command).ToArray()[0];
+        }
+    }
+
+    void TwitchHandleForcedSolve(){
+        Debug.LogFormat("[Q & A #{0}] Force solved by Twitch mod.", moduleId);
+        if(isSolved)
+            return;
+        else{
+            int i = 0;
+            while(!isSolved && i < 10){
+                if(display_text.text == ""){
+                    pressAnswer(7);
+                }
+                else{
+                    pressAnswer(System.Array.IndexOf(answers, c_answers[table[System.Array.IndexOf(c_answers, previous) * 6 + System.Array.IndexOf(c_questions, display_text.text)]]));
+                }
+                i++;
+            }
+        }
+    }
 }
