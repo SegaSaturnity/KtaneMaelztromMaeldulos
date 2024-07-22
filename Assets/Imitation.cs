377eddc1add81425cb7f4e1f86f9969b7675a1f9
ObjectsCountries <oandcofficial@gmail.com>
2024-07-21 19:47:47 -0700

    Begin development on Imitation

---
+++ Assets/Imitation.cs
@@ -0,0 +1,20 @@
+using Wawa.Modules;
+using Wawa.Extensions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KModkit;
+
+public class Imitation : ModdedModule
+{
+    public KMRuleSeedable rs;
+    public KMSelectable sendButton;
+    public KMSelectable[] dialButtons;
+
+    public void Start()
+    {
+
+    }
+}
